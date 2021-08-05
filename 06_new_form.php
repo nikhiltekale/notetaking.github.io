bde11754346f3e9f9ef676c70368877dd0a5cde9
Tekalenikhil <nikhiltekale63@gmail.com>
2021-08-05 18:57:50 +0530

    todolist

---
+++ 06_new_form.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>contact</title>
+  </head>
+  <body>
+    
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/PHP_TUT_BY_CWH/06_new_form.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<?php
+
+
+  if($_SERVER['REQUEST_METHOD']=='POST'){
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $desc = $_POST['desc'];
+     
+      //connection to database
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $database="registration_form_01";
+   
+    //Create Connection
+    $conn = mysqli_connect($servername, $username, $password,$database);
+   
+    //Die if connection was not success
+    if(!$conn)
+    {
+        die("sry we field to connect:".mysqli_connect_error());
+   
+    }
+    else{
+    //    echo"connection was successful";
+     
+       //insert data into table
+        
+       $sql=" INSERT INTO `information_01` ( `name`, `email`, `concern`, `date`)
+        VALUES ( '$name', '$email', '$desc', current_timestamp());";
+        $result=mysqli_query($conn,$sql);
+
+        if($result)
+        {
+            // echo"table create successfully..<br>";
+
+            echo'<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>your data is successfully submited </strong><br>
+            <span> if u want to insert another responce then cleak on:<button href="/PHP_TUT_BY_CWH/06_new_form.php">HOME</button></span>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>';
+      
+
+
+        }
+        else{
+            echo"table creation faield".mysqli_error($conn);
+        }
+
+    }
+   
+  }
+
+
+?>
+
+<div class="container">
+<h1>Registration form</h1>
+<form action="/PHP_TUT_BY_CWH/06_new_form.php" method="POST">
+<div class="form-group">
+    <label for="name">name</label>
+    <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp" placeholder="Enter email">
+    <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+  </div>
+   
+  <div class="form-group">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="desc">Discription</label>
+    <textarea class="form-control" column="30" rows="10" name="desc" id="desc" ></textarea>
+  </div>
+  <!-- <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div> -->
+  <button type="submit" class="btn btn-primary" >Submit</button>
+</form>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
